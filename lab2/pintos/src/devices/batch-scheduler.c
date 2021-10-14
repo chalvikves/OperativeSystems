867519003c970d8bdebd2c72dbdb9dd1a00f3662
chalvikves <vikves@student.chalmers.se>
2021-10-14 16:33:27 +0200

    hoppas

--- lab2/pintos/src/devices/batch-scheduler.c
+++ lab2/pintos/src/devices/batch-scheduler.c
@@ -19,22 +19,25 @@
  *	initialize task with direction and priority
  *	call o
  * */
-typedef struct {
-	int direction;
-	int priority;
+typedef struct
+{
+    int direction;
+    int priority;
 } task_t;
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-        unsigned int num_priority_send, unsigned int num_priority_receive);
+                    unsigned int num_priority_send, unsigned int num_priority_receive);
 
 void senderTask(void *);
 void receiverTask(void *);
 void senderPriorityTask(void *);
 void receiverPriorityTask(void *);
 
-struct semaphore waitingToTransfer;         /* The normal tasks waiting for the bus */
-struct semaphore prioWaitingToTransfer;     /* The prioritized tasks waiting for the bus */
-struct semaphore fullBus;                   /* If the bus is full we need to wait */
+struct semaphore sendWaitingToTransfer;     /* The sending normal tasks waiting for the bus */
+struct semaphore sendPrioWaitingToTransfer; /* The sending prioritized tasks waiting for the bus */
+struct semaphore recWaitingToTransfer;      /* The recieving normal tasks waiting for the bus */
+struct semaphore recPrioWaitingToTransfer;  /* The recieving prioritized tasks waiting for the bus */
+struct semaphore busSem;                    /* If the bus is full we need to wait */
 struct lock lock;                           /* The normal lock */
 struct lock prioLock;                       /* The priority lock */
 int waiters[2];                             /* Number of waiters on each side */
@@ -42,32 +45,31 @@ int prioWaiters[2];                         /* Number of prioritized waiters on 
 int runningTasks;                           /* Number of tasks in the bus */
 int currentDirection;                       /* The direction of the bus SENDER/RECIEVER */
 
-
-void oneTask(task_t task);/*Task requires to use the bus and executes methods below*/
-void getSlot(task_t task); /* task tries to use slot on the bus */
+void oneTask(task_t task);      /*Task requires to use the bus and executes methods below*/
+void getSlot(task_t task);      /* task tries to use slot on the bus */
 void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
-void leaveSlot(task_t task); /* task release the slot */
+void leaveSlot(task_t task);    /* task release the slot */
 
+/* initializes semaphores */
+void init_bus(void)
+{
 
-
-/* initializes semaphores */ 
-void init_bus(void){ 
- 
-    random_init((unsigned int)123456789); 
+    random_init((unsigned int)123456789);
 
     // Init semaphores
-    sema_init(&waitingToTransfer, 1);
-    sema_init(&prioWaitingToTransfer, 1);
-    sema_init(&fullBus, BUS_CAPACITY);
+    sema_init(&sendWaitingToTransfer, 1);
+    sema_init(&sendPrioWaitingToTransfer, 1);
+    sema_init(&recWaitingToTransfer, 1);
+    sema_init(&recPrioWaitingToTransfer, 1);
+    sema_init(&busSem, BUS_CAPACITY);
 
     // Init locks
     lock_init(&lock);
-    lock_init(&prioLock); 
+    lock_init(&prioLock);
 
     // Initvariables
     runningTasks = 0;
     currentDirection = 0;
-
 }
 
 /*
@@ -82,131 +84,206 @@ void init_bus(void){
  */
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-        unsigned int num_priority_send, unsigned int num_priority_receive)
+                    unsigned int num_priority_send, unsigned int num_priority_receive)
 {
     unsigned int i;
     /* create sender threads */
-    for(i = 0; i < num_tasks_send; i++)
+    for (i = 0; i < num_tasks_send; i++)
         thread_create("sender_task", 1, senderTask, NULL);
 
     /* create receiver threads */
-    for(i = 0; i < num_task_receive; i++)
+    for (i = 0; i < num_task_receive; i++)
         thread_create("receiver_task", 1, receiverTask, NULL);
 
     /* create high priority sender threads */
-    for(i = 0; i < num_priority_send; i++)
-       thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
+    for (i = 0; i < num_priority_send; i++)
+        thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
 
     /* create high priority receiver threads */
-    for(i = 0; i < num_priority_receive; i++)
-       thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
+    for (i = 0; i < num_priority_receive; i++)
+        thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
 }
 
 /* Normal task,  sending data to the accelerator */
-void senderTask(void *aux UNUSED){
-        task_t task = {SENDER, NORMAL};
-        oneTask(task);
+void senderTask(void *aux UNUSED)
+{
+    task_t task = {SENDER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, sending data to the accelerator */
-void senderPriorityTask(void *aux UNUSED){
-        task_t task = {SENDER, HIGH};
-        oneTask(task);
+void senderPriorityTask(void *aux UNUSED)
+{
+    task_t task = {SENDER, HIGH};
+    oneTask(task);
 }
 
 /* Normal task, reading data from the accelerator */
-void receiverTask(void *aux UNUSED){
-        task_t task = {RECEIVER, NORMAL};
-        oneTask(task);
+void receiverTask(void *aux UNUSED)
+{
+    task_t task = {RECEIVER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, reading data from the accelerator */
-void receiverPriorityTask(void *aux UNUSED){
-        task_t task = {RECEIVER, HIGH};
-        oneTask(task);
+void receiverPriorityTask(void *aux UNUSED)
+{
+    task_t task = {RECEIVER, HIGH};
+    oneTask(task);
 }
 
 /* abstract task execution*/
-void oneTask(task_t task) {
-  getSlot(task);
-  transferData(task);
-  leaveSlot(task);
+void oneTask(task_t task)
+{
+    getSlot(task);
+    transferData(task);
+    leaveSlot(task);
 }
 
-
 /* task tries to get slot on the bus subsystem */
-void getSlot(task_t task) 
+void getSlot(task_t task)
 {
 
-
-    if(currentDirection != task.direction && runningTasks < BUS_CAPACITY)
+    if (task.direction == SENDER)
     {
-        sema_down(&fullBus);
-    }
+        lock_acquire(&lock);
+        // Priority tasks
+        if (task.priority == HIGH)
+        {
 
-    
-    // Priority tasks
-    if(task.priority == HIGH){
-        lock_acquire(&lock);
-        lock_acquire(&prioLock);
-        while((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction )){
-            prioWaiters[task.direction]++;
-            sema_down(&prioWaitingToTransfer);
-            prioWaiters[task.direction]--;
+            while ((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction))
+            {
+
+                prioWaiters[task.direction]++;
+                sema_down(&sendPrioWaitingToTransfer);
+                sema_down(&busSem);
+                prioWaiters[task.direction]--;
+            }
         }
-        lock_release(&prioLock);
+        // non-priority tasks
+        else
+        {
+            lock_acquire(&lock);
+            lock_acquire(&prioLock);
+            while ((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction))
+            {
+                waiters[task.direction]++;
+                sema_down(&sendWaitingToTransfer);
+                sema_down(&busSem);
+                waiters[task.direction]--;
+            }
+            lock_release(&prioLock);
+        }
     }
-    // non-priority tasks
-    else {
+    else
+    {
         lock_acquire(&lock);
-        while((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction )){
-            waiters[task.direction]++;
-            sema_down(&waitingToTransfer);
-            waiters[task.direction]--;
+        // Priority tasks
+        if (task.priority == HIGH)
+        {
+
+            while ((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction))
+            {
+
+                prioWaiters[task.direction]++;
+                sema_down(&recPrioWaitingToTransfer);
+                sema_down(&busSem);
+                prioWaiters[task.direction]--;
+            }
+        }
+        // non-priority tasks
+        else
+        {
+            lock_acquire(&lock);
+            lock_acquire(&prioLock);
+            while ((runningTasks == BUS_CAPACITY) || (runningTasks > 0 && currentDirection != task.direction))
+            {
+                waiters[task.direction]++;
+                sema_down(&recWaitingToTransfer);
+                sema_down(&busSem);
+                waiters[task.direction]--;
+            }
+            lock_release(&prioLock);
         }
     }
     runningTasks++;
     currentDirection = task.direction;
     lock_release(&lock);
-    
-    
-    
 }
 
 /* task processes data on the bus send/receive */
-void transferData(task_t task) 
+void transferData(task_t task)
 {
     timer_sleep(random_ulong() % 10);
 }
 
 /* task releases the slot */
-void leaveSlot(task_t task) 
-{   
+void leaveSlot(task_t task)
+{
     lock_acquire(&lock);
     runningTasks--;
 
-    if (task.priority){
-        lock_acquire(&prioLock);
-        if (prioWaiters[currentDirection] > 0){
-            sema_up(&prioWaitingToTransfer);
-        }
-        else if (prioWaiters[1-currentDirection] > 0){
-            if (runningTasks == 0){
-                sema_up(&prioWaitingToTransfer);
+    if (task.direction == SENDER)
+    {
+        if (task.priority)
+        {
+
+            if (prioWaiters[currentDirection] > 0)
+            {
+                sema_up(&sendPrioWaitingToTransfer);
+            }
+            else if (prioWaiters[1 - currentDirection] > 0)
+            {
+                if (runningTasks == 0)
+                {
+                    sema_up(&sendPrioWaitingToTransfer);
+                }
             }
         }
-        lock_release(&prioLock);
-    } else {
-        if (waiters[currentDirection] > 0){
-        sema_up(&waitingToTransfer);
-        }
-        else if (runningTasks == 0){
-            sema_up(&waitingToTransfer);
+        else
+        {
+            lock_acquire(&prioLock);
+            if (waiters[currentDirection] > 0)
+            {
+                sema_up(&sendWaitingToTransfer);
+            }
+            else if (runningTasks == 0)
+            {
+                sema_up(&sendWaitingToTransfer);
+            }
+            lock_release(&prioLock);
         }
     }
-    
-    if(runningTasks == 0){
-        sema_up(&fullBus);
+    else
+    {
+        if (task.priority)
+        {
+
+            if (prioWaiters[currentDirection] > 0)
+            {
+                sema_up(&recPrioWaitingToTransfer);
+            }
+            else if (prioWaiters[1 - currentDirection] > 0)
+            {
+                if (runningTasks == 0)
+                {
+                    sema_up(&recPrioWaitingToTransfer);
+                }
+            }
+        }
+        else
+        {
+            lock_acquire(&prioLock);
+            if (waiters[currentDirection] > 0)
+            {
+                sema_up(&recWaitingToTransfer);
+            }
+            else if (runningTasks == 0)
+            {
+                sema_up(&recWaitingToTransfer);
+            }
+            lock_release(&prioLock);
+        }
     }
 
     lock_release(&lock);
